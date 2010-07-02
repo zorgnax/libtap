c9a7b8ad242ea17231a257f783597413d2b1cc4c
jake <zorgnax@gmail.com>
2010-07-01 20:37:57 -0400

    Indent nicer in tap.h.

--- tap.h
+++ tap.h
@@ -16,35 +16,35 @@ int  note          (const char *fmt, ...);
 int  exit_status   (void);
 
 #ifdef _WIN32
-#define dies_ok_common(code, for_death, ...) \
-    ok(1, "# skip unable to test if code dies on Windows");
+#   define dies_ok_common(code, for_death, ...) \
+        ok(1, "# SKIP unable to test if code dies on windows");
 #else
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-int tap_test_died (int status);
-#define dies_ok_common(code, for_death, ...)                \
-    {                                                       \
-        tap_test_died(1);                                   \
-        int cpid = fork();                                  \
-        switch (cpid) {                                     \
-        case -1:                                            \
-            perror("fork error");                           \
-            exit(EXIT_FAILURE);                             \
-        case 0: /* child  */                                \
-            close(1); close(2);                             \
-            code                                            \
-            tap_test_died(0);                               \
-            exit(EXIT_SUCCESS);                             \
-        }                                                   \
-        if (waitpid(cpid, NULL, 0) < 0) {                   \
-            perror("waitpid error");                        \
-            exit(EXIT_FAILURE);                             \
-        }                                                   \
-        int it_died = tap_test_died(0);                     \
-        if (!it_died) {code}                                \
-        ok(for_death ? it_died : !it_died, ## __VA_ARGS__); \
-    }
+#   include <unistd.h>
+#   include <sys/types.h>
+#   include <sys/wait.h>
+    int tap_test_died (int status);
+#   define dies_ok_common(code, for_death, ...)                \
+        {                                                       \
+            tap_test_died(1);                                   \
+            int cpid = fork();                                  \
+            switch (cpid) {                                     \
+            case -1:                                            \
+                perror("fork error");                           \
+                exit(EXIT_FAILURE);                             \
+            case 0: /* child  */                                \
+                close(1); close(2);                             \
+                code                                            \
+                tap_test_died(0);                               \
+                exit(EXIT_SUCCESS);                             \
+            }                                                   \
+            if (waitpid(cpid, NULL, 0) < 0) {                   \
+                perror("waitpid error");                        \
+                exit(EXIT_FAILURE);                             \
+            }                                                   \
+            int it_died = tap_test_died(0);                     \
+            if (!it_died) {code}                                \
+            ok(for_death ? it_died : !it_died, ## __VA_ARGS__); \
+        }
 #endif
 
 #define dies_ok(code, ...)  dies_ok_common(code, 1, ## __VA_ARGS__)
