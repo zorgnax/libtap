cc8d62fb3e0725ca84ad705e085264f5baff122c
jake <zorgnax@gmail.com>
2010-07-22 00:50:58 -0400

    Write dies_ok differently to avoid the compilor warning.

--- t/diesok.c
+++ t/diesok.c
@@ -4,7 +4,7 @@ int main () {
     setvbuf(stdout, NULL, _IONBF, 0);
     plan(5);
     ok(1, "sanity");
-    dies_ok({int x; x = 0/0;}, "can't divide by zero");
+    dies_ok({int x = 0; x = x/x;}, "can't divide by zero");
     lives_ok({int x; x = 3/7;}, "this is a perfectly fine statement");
     dies_ok({abort();}, "abort kills the program");
     dies_ok(
