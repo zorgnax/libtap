5745d536d62201fae74d5b04196117a5fad8eb00
jake <zorgnax@gmail.com>
2010-07-21 21:33:54 -0400

    Furthur format the style of the code.

--- tap.h
+++ tap.h
@@ -12,10 +12,7 @@
 #define isnt(...)        isnt_at_loc(__FILE__, __LINE__, __VA_ARGS__, NULL)
 
 void    plan            (int tests);
-int     ok_at_loc       (const char *file,
-                         int         line,
-                         int         test,
-                         const char *fmt,
+int     ok_at_loc       (const char *file, int line, int test, const char *fmt,
                          ...);
 int     diag            (const char *fmt, ...);
 int     note            (const char *fmt, ...);
@@ -23,32 +20,20 @@ int     exit_status     (void);
 void    skippy          (int n, const char *fmt, ...);
 void    ctodo           (int ignore, const char *fmt, ...);
 void    cendtodo        (void);
-int     is_at_loc       (const char *file,
-                         int         line,
-                         const char *got,
-                         const char *expected,
-                         const char *fmt,
-                         ...);
-int     isnt_at_loc     (const char *file,
-                         int         line,
-                         const char *got,
-                         const char *expected,
-                         const char *fmt,
-                         ...);
+int     is_at_loc       (const char *file, int line, const char *got,
+                         const char *expected, const char *fmt, ...);
+int     isnt_at_loc     (const char *file, int line, const char *got,
+                         const char *expected, const char *fmt, ...);
 
 #ifdef _WIN32
-#   define like(...)   skippy(1, "like is not implemented on MSWin32")
-#   define unlike(...) like()
+#define like(...)   skippy(1, "like is not implemented on MSWin32")
+#define unlike(...) like()
 #else
-#   define like(...)   like_at_loc(1, __FILE__, __LINE__, __VA_ARGS__, NULL)
-#   define unlike(...) like_at_loc(0, __FILE__, __LINE__, __VA_ARGS__, NULL)
-    int     like_at_loc     (int         for_match,
-                             const char *file,
-                             int         line,
-                             const char *got,
-                             const char *expected,
-                             const char *fmt,
-                             ...);
+#define like(...)   like_at_loc(1, __FILE__, __LINE__, __VA_ARGS__, NULL)
+#define unlike(...) like_at_loc(0, __FILE__, __LINE__, __VA_ARGS__, NULL)
+int     like_at_loc     (int for_match, const char *file, int line,
+                         const char *got, const char *expected,
+                         const char *fmt, ...);
 #endif
 
 #define skip(test, ...)  do {if (test) {skippy(__VA_ARGS__, NULL); break;}
@@ -61,35 +46,34 @@ int     isnt_at_loc     (const char *file,
 #define lives_ok(code, ...) dies_ok_common(code, 0, ## __VA_ARGS__)
 
 #ifdef _WIN32
-#   define dies_ok_common(...) \
-        skippy(1, "Death detection is not supported on MSWin32")
+#define dies_ok_common(...) \
+    skippy(1, "Death detection is not supported on MSWin32")
 #else
-#   include <unistd.h>
-#   include <sys/types.h>
-#   include <sys/wait.h>
-    int tap_test_died (int status);
-#   define dies_ok_common(code, for_death, ...)                 \
-        do {                                                    \
-            tap_test_died(1);                                   \
-            int cpid = fork();                                  \
-            switch (cpid) {                                     \
-            case -1:                                            \
-                perror("fork error");                           \
-                exit(EXIT_FAILURE);                             \
-            case 0: /* child  */                                \
-                close(1); close(2);                             \
-                code                                            \
-                tap_test_died(0);                               \
-                exit(EXIT_SUCCESS);                             \
-            }                                                   \
-            if (waitpid(cpid, NULL, 0) < 0) {                   \
-                perror("waitpid error");                        \
-                exit(EXIT_FAILURE);                             \
-            }                                                   \
-            int it_died = tap_test_died(0);                     \
-            if (!it_died) {code}                                \
-            ok(for_death ? it_died : !it_died, ## __VA_ARGS__); \
-        } while (0)
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int tap_test_died (int status);
+#define dies_ok_common(code, for_death, ...)                \
+    do {                                                    \
+        tap_test_died(1);                                   \
+        int cpid = fork();                                  \
+        switch (cpid) {                                     \
+        case -1:                                            \
+            perror("fork error");                           \
+            exit(EXIT_FAILURE);                             \
+        case 0: /* child  */                                \
+            close(1); close(2);                             \
+            code                                            \
+            tap_test_died(0);                               \
+            exit(EXIT_SUCCESS);                             \
+        }                                                   \
+        if (waitpid(cpid, NULL, 0) < 0) {                   \
+            perror("waitpid error");                        \
+            exit(EXIT_FAILURE);                             \
+        }                                                   \
+        int it_died = tap_test_died(0);                     \
+        if (!it_died) {code}                                \
+        ok(for_death ? it_died : !it_died, ## __VA_ARGS__); \
+    } while (0)
 #endif
-
 #endif
