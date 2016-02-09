d2109aa9d3898359bb8c5f8dabe0221169ed4bd2
Jacob Gelbman <gelbman@gmail.com>
2016-02-08 21:00:49 -0600

    Use x variable so compiler doesn't complain about it

--- t/diesok.c
+++ t/diesok.c
@@ -4,7 +4,7 @@ int main () {
     plan(5);
     ok(1, "sanity");
     dies_ok({int x = 0; x = x/x;}, "can't divide by zero");
-    lives_ok({int x; x = 3/7;}, "this is a perfectly fine statement");
+    lives_ok({int x = 3; x = x/7;}, "this is a perfectly fine statement");
     dies_ok({abort();}, "abort kills the program");
     dies_ok(
         {printf("stdout\n"); fprintf(stderr, "stderr\n"); abort();},
